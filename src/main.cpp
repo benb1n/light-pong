ee74f9e83bf0747a79b982a373482d62308a6e27
Ben Ellis <ben@elliscloud.dev>
2023-07-04 14:19:28 -0500

    Add OLED code but comment out

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 #include <ezButton.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Fonts/FreeSansBold9pt7b.h>
 #ifdef __AVR__
  #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
@@ -11,6 +16,17 @@
 #define LED_COUNT 57
 
 #define DEBOUNCE_TIME 50
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
@@ -52,10 +68,31 @@ int LightAfter = CurrentLight + 1;
 //   }
 // }
 
-// void IRAM_ATTR yurMom()
-// {
-//   Test_Button = true;
-// }
+void initDisplay() {
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(1000);
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println("Welcome to Light Pong");
+  display.display();
+  delay(1000);
+
+  display.setFont(&FreeSansBold9pt7b);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 40);
+  display.println("Ready player 1");
+  display.display();
+}
 
 void setup() {
 
@@ -67,8 +104,10 @@ void setup() {
   clock_prescale_set(clock_div_1);
 #endif
   // END of Trinket-specific code.
- // pinMode(pushButton_pin, INPUT_PULLUP);
+  // pinMode(pushButton_pin, INPUT_PULLUP);
   //attachInterrupt(pushButton_pin, yurMom, FALLING);
+
+  //initDisplay();
 
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all pixels ASAP

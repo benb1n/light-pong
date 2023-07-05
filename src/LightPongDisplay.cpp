80f9ffd66b4acd489a4ac94637e5858569a809ad
Ben Ellis <ben@elliscloud.dev>
2023-07-04 22:48:34 -0500

    Move display code to separate file

---
+++ src/LightPongDisplay.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Fonts/FreeSansBold9pt7b.h>
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+void initDisplay() {
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(1000);
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println("Welcome to Light Pong");
+  display.display();
+  delay(1000);
+
+  display.setFont(&FreeSansBold9pt7b);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 40);
+  display.println("Ready player 1");
+  display.display();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,38 +3,23 @@
 #include <ezButton.h>
 #include <SPI.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <Fonts/FreeSansBold9pt7b.h>
 #ifdef __AVR__
  #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
 
-#define LED_PIN    5
+#include "LightPongDisplay.h"
 
+#define LED_PIN    5
 // How many NeoPixels are attached to the Arduino?
 #define LED_COUNT 57
-
 #define DEBOUNCE_TIME 50
-
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-// The pins for I2C are defined by the Wire-library. 
-// On an arduino UNO:       A4(SDA), A5(SCL)
-// On an arduino MEGA 2560: 20(SDA), 21(SCL)
-// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-// Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-int Forward_Button_Pin = 4;//15;
+int Forward_Button_Pin = 4;
 int Backward_Button_Pin = 2;
 ezButton Forward_Button(Forward_Button_Pin);
 ezButton Backward_Button(Backward_Button_Pin);
+
 int speedOfReturn = 50;
 int CurrentLight = 0;
 bool Moving_Forward = true;
@@ -48,31 +33,7 @@ int LightBefore = CurrentLight - 1;
 int LightAfter = CurrentLight + 1;
 
 
-void initDisplay() {
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
 
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  display.display();
-  delay(1000);
-  display.clearDisplay();
-
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.println("Welcome to Light Pong");
-  display.display();
-  delay(1000);
-
-  display.setFont(&FreeSansBold9pt7b);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 40);
-  display.println("Ready player 1");
-  display.display();
-}
 
 void setup() {
 
@@ -95,7 +56,6 @@ void setup() {
   
   Forward_Button.setDebounceTime(DEBOUNCE_TIME);
 }
-
 
 
 void loop()
@@ -172,45 +132,4 @@ void loop()
     }
   }
 }
-
-// void old_dry_bones_loop() {
-
-
-
-//   if (Is_Beginning == true)
-//   {
-//    colorWipe(strip.Color(255,   0,   0), strip.Color(0, 0, 0), speedOfReturn);
-//   }
-//   Is_Beginning = false;
-
-//   if (Test_Button == true)
-//   {
-//     Test_Button = false;
-//     if (Moving_Forward == true)
-//     {
-//       colorWipeReverse(strip.Color(255,   0,   0), strip.Color(0, 0, 0), speedOfReturn); // Red
-//       Moving_Forward = false;
-//     }
-//     else
-//     {
-//       colorWipe(strip.Color(255,   0,   0), strip.Color(0, 0, 0), speedOfReturn);
-//       Moving_Forward = true;
-//     }
-//   }
-
-
-//   // Fill along the length of the strip in various colors...
-//   //colorWipe(strip.Color(255,   0,   0), strip.Color(0, 0, 0), speedOfReturn); // Red
-//   //speedOfReturn = speedOfReturn / 2;
-//   //colorWipeReverse(strip.Color(255,   0,   0), strip.Color(0, 0, 0), speedOfReturn); // Red
-
-  
-  
-//   // colorWipe(strip.Color(0,   0,   0), 50); // Red
-//   // colorWipeReverse(strip.Color(255,   0,   0), 50); // Red
-//   // colorWipeReverse(strip.Color(0,   0,   0), 50); // Red
-// }
-   
-
-
 
